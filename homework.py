f3bab72113e21884323102976ef69bc95b363483
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-06-24 23:41:56 +0300

    finish

--- homework.py
+++ homework.py
@@ -9,6 +9,8 @@ import requests
 from dotenv import load_dotenv
 from telegram import Bot
 
+from exceptions import BotException
+
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
@@ -25,34 +27,51 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-handler = logging.StreamHandler(sys.stdout)
-logging.basicConfig(
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    level=logging.INFO,
-    handlers=[handler],
+BOT = Bot(token=TELEGRAM_TOKEN)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s'
 )
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
 
 
 def send_message(bot, message):
-    return bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    """Отправка сообщения в телеграм."""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logging.info('Abgesendet!')
+    except BotException:
+        logging.error('Nicht abgesendet!')
+        return 'Nicht abgesendet!'
 
 
 def get_api_answer(current_timestamp):
-    try:
-        params = {'from_date': current_timestamp}
-        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-        return response.json()
-    except requests.exceptions.RequestException:
-        logging.error(f'{response.status_code}')
-
+    """Запрос к API Яндекса."""
+    params = {'from_date': current_timestamp}
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    if response.status_code != HTTPStatus.OK:
+        logging.error(
+            f'Шеф, все пропало {response.status_code}...')
+        send_message(BOT, f'Шеф, все пропало {response.status_code}...')
+        raise BotException('Bad, really bad requests')
+    return response.json()
 
 
 def check_response(response):
+    """Возврат работ и проверка на корректность данных."""
+    if not isinstance(response['homeworks'], list):
+        logging.error('Где же мой лист?!')
+        send_message(BOT, 'Прости, но лист я так и не нашел...')
+        raise BotException('Keine Liste, keine Probleme')
     return response['homeworks']
 
 
 def parse_status(homework):
+    """Проверка статуса работы."""
     homework_name = homework['homework_name']
     homework_status = homework['status']
     verdict = HOMEWORK_STATUSES[homework_status]
@@ -60,6 +79,7 @@ def parse_status(homework):
 
 
 def check_tokens():
+    """Проверка переменных среды."""
     if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
         logging.info('Es geht!')
         return True
@@ -70,21 +90,19 @@ def main():
     """Основная логика работы бота."""
     if not check_tokens():
         return False
-
     while True:
         try:
-            bot = Bot(token=TELEGRAM_TOKEN)
             current_timestamp = int(time.time())
             response = get_api_answer(current_timestamp)
-            print(response)
             homeworks = check_response(response)
-            send_message(bot, parse_status(homeworks[0]))
+            if len(homeworks) > 0:
+                send_message(BOT, parse_status(homeworks[0]))
+            logging.info('Es gibt keine Ausgaben')
+            time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            send_message(bot, message)
+            send_message(BOT, message)
             time.sleep(RETRY_TIME)
-        # else:
-        #     ...
 
 
 if __name__ == '__main__':
